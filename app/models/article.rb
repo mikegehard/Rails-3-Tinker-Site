c00ffae137138fb45f8e2654a092fdbfea8cbcc3
Mike Gehard <mike.gehard@gmail.com>
2011-04-10 11:31:04 -0600

    Allow an article to have many tags.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   validates :headline, :presence => true
+
+  has_many :article_tags
+  has_many :tags, :through => :article_tags
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Article do
+  it { should have_many(:tags).through(:article_tags) }
+  
   it "should require headline" do
     subject.headline = ""
     subject.should_not be_valid
