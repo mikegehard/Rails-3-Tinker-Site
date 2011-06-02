edd720c821c34cfa2be820a5f68f2a8dc23cbd04
Mike Gehard <mike.gehard@gmail.com>
2011-06-01 21:25:35 -0600

    No longer needed in Rails 3.1 rc

--- spec/support/rack_patch.rb
+++
@@ -1,8 +0,0 @@
-#see http://stackoverflow.com/questions/5614661/capybara-submit-button-incompatible-encoding-regexp-match
-# for more info
-
-module Rack::Utils
-  def escape(s)
-    CGI.escape(s.to_s)
-  end
-end
